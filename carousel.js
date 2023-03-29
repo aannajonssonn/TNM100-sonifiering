a3b6d9be022020d9ff1fbafd77b82b7f44db4ea2
Anna Jonsson <annjo712@student.liu.se>
2023-03-29 17:06:07 +0200

    Started implementing some functionalities
    such as graphs, buttons, and menus

---
+++ carousel.js
@@ -0,0 +1,47 @@
+// Source code: https://codepen.io/frise/pen/mZvKpe 
+
+const state = {}
+const carouselList = document.querySelectorAll('.carousel__list')
+
+carouselList.forEach((list) => {
+  list.addEventListener('click', function (event) {
+    var newActive = event.target
+    var isItem = newActive.closest('.carousel__item')
+  
+    if (!isItem || newActive.classList.contains('carousel__item_active')) {
+      return
+    }
+    
+    update(newActive, list.children)
+  })
+})
+
+const update = function(newActive, listElement) {
+  const newActivePos = newActive.dataset.pos
+  // Parse from HTMLCollection to Array
+  var listElements = Array.prototype.slice.call(listElement)
+
+  const current = listElements.find((elem) => elem.dataset.pos == 0)
+  const prev = listElements.find((elem) => elem.dataset.pos == -1)
+  const next = listElements.find((elem) => elem.dataset.pos == 1)
+  const first = listElements.find((elem) => elem.dataset.pos == -2)
+  const last = listElements.find((elem) => elem.dataset.pos == 2)
+  
+  current.classList.remove('carousel__item_active');
+  
+  [current, prev, next, first, last].forEach(item => {
+    var itemPos = item.dataset.pos
+
+    item.dataset.pos = getPos(itemPos, newActivePos)
+  })
+}
+
+const getPos = function (current, active) {
+  const diff = current - active
+
+  if (Math.abs(current - active) > 2) {
+    return -current
+  }
+
+  return diff
+}
---
+++ datasets.js
@@ -0,0 +1,12 @@
+
+let data = ['Dataset 1', 'Dataset 2', 'Dataset 3', 'Dataset 4' ];
+ 
+let list = document.getElementById("datasets_list");
+ 
+data.forEach((item)=>{
+  let li = document.createElement("li");
+  var button = document.createElement("button");
+  button.innerText = item;
+  li.appendChild(button);
+  list.appendChild(li);
+})
