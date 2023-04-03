a9f27f01dcdea99257b86ea1091172123d334fdf
Casper Larsson <casper.larsson@live.se>
2023-04-03 12:13:46 +0200

    Changed colours and added icons for carousel menu

--- js/datasets.js
+++ js/datasets.js
@@ -1,13 +1,41 @@
+// const data = ['Dataset 1', 'Dataset 2', 'Dataset 3', 'Dataset 4'] // namnen på de olika dataseten
 
-let data = ['Dataset 1', 'Dataset 2', 'Dataset 3', 'Dataset 4' ];
- 
-let list = document.getElementById("datasets_list");
- 
-data.forEach((item)=>{
-  let li = document.createElement("li");
-  var button = document.createElement("button");
-  button.innerText = item;
-  button.onclick = '';
-  li.appendChild(button);
-  list.appendChild(li);
+const dataOptions = [{
+  label: 'Dataset 1',
+  datalink: './data/temperature.csv'
+},
+{
+  label: 'Dataset 2',
+  datalink: './data/testdata.csv'
+},
+{
+  label: 'Dataset 3',
+  datalink: './data/testdata.csv'
+},
+{
+  label: 'Dataset 4',
+  datalink: './data/testdata.csv'
+}
+]
+
+const list = document.getElementById("datasets")
+
+dataOptions.forEach((item) => {
+  const div = document.createElement("div")
+  div.innerText = item.label
+  div.className = 'onoff'
+
+  const label = document.createElement('label')
+  label.className = 'switch'
+
+  const input = document.createElement('input')
+  input.type = 'checkbox'
+  //input.id = ''
+  const span = document.createElement('span')
+  span.className = 'slider round'
+
+  input.appendChild(span)
+  label.appendChild(input)
+  div.appendChild(label)
+  list.appendChild(div)
 })
