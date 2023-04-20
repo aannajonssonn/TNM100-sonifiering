9b043c9668d87a6efd72656da5838989d8db787d
Casper Larsson <73782366+casperlarsson-bit@users.noreply.github.com>
2023-04-20 16:10:02 +0200

    Made better styling for play button

--- js/playbutton.js
+++ js/playbutton.js
@@ -1,12 +1,18 @@
-document.getElementById('play').onclick = function () {
+const playButton = document.getElementById('play')
+function slideRange () {
+    playButton.removeEventListener('click', slideRange, false)
     const range = document.getElementById('play-range')
-
+    playButton.classList.add('clicked')
+    playButton.style.cursor = 'context-menu'
     timeout(parseInt(range.min), range)
 }
 
 function timeout(i, range) {
     if (parseInt(i) > range.max) {
         range.value = range.min
+        playButton.style.cursor = 'pointer'
+        playButton.classList.remove('clicked')
+        playButton.addEventListener('click', slideRange, false)
         return
     }
     
@@ -16,3 +22,5 @@ function timeout(i, range) {
         timeout(parseInt(i+1), range)
     }, 25)
 }
+
+playButton.addEventListener('click', slideRange, false)
