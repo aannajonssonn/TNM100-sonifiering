9286ba5b62266184c30cfac0660029382ae3fbac
IvarGorenko <73782886+IvarGorenko@users.noreply.github.com>
2023-05-03 23:16:45 +0200

    onoff med endast 1 reglage åt gången

--- js/onoff.js
+++ js/onoff.js
@@ -2,24 +2,49 @@
 const dataset1 = document.querySelector('#dataset1-button')
 const dataset2 = document.querySelector('#dataset2-button')
 const dataset3 = document.querySelector('#dataset3-button')
-const dataset_element = document.querySelectorAll('#dataset1-button, #dataset2-button, #dataset3-button')
 console.log(dataset1)
 // array of datasets
-const datasets = [[dataset_element[0], 'temperature', '/data/temperature.csv'], [dataset_element[1], 'cars', '/data/cars.csv'], [dataset_element[2], 'people', '/data/befolkning.csv']]
-console.log(datasets)
+const datasets = [[dataset1, 'temperature', '/data/temperature.csv'], [dataset2, 'cars', '/data/cars.csv'], [dataset3, 'people', '/data/befolkning.csv']]
 // listening to the event listener and sending the csv link, category and index to the drawGraph function for the checked dataset
 
 
 // if a change is detected in any checkbox, all graphs are redrawn
-datasets.forEach((dataset, index) => {
-  dataset[0].addEventListener('change', () => {
-    // remove all graphs using datasets using removeGraph function
-    removeGraph(dataset[1])
-    // if the checkbox is checked, draw the graph
-    if (dataset[0].checked) {
-      drawGraph(dataset[2], dataset[1], index)
-    }
-  })
+dataset1.addEventListener('change', function () {
+  if (dataset1.checked) {
+    drawGraph(datasets[0][2], datasets[0][1], 0)
+    // uncheck the other checkboxes, remove the graphs
+    dataset2.checked = false
+    dataset3.checked = false
+    removeGraph(datasets[1][1])
+    removeGraph(datasets[2][1])
+  } else {
+    removeGraph(datasets[0][1])
+  }
+})
+
+// repeat for the other checkboxes
+dataset2.addEventListener('change', function () {
+  if (dataset2.checked) {
+    drawGraph(datasets[1][2], datasets[1][1], 1)
+    dataset1.checked = false
+    dataset3.checked = false
+    removeGraph(datasets[0][1])
+    removeGraph(datasets[2][1])
+  } else {
+    removeGraph(datasets[1][1])
+  }
+})
+
+dataset3.addEventListener('change', function () {
+  if (dataset3.checked) {
+    drawGraph(datasets[2][2], datasets[2][1], 2)
+    dataset1.checked = false
+    dataset2.checked = false
+    removeGraph(datasets[0][1])
+    removeGraph(datasets[1][1])
+  } else {
+    removeGraph(datasets[2][1])
+  }
 })
 
 
